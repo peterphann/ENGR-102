fdc9c50557cf476233d3435867ee618ca3349d5f
Peter <49101240+peterphann@users.noreply.github.com>
2023-10-16 21:17:45 -0500

    Update ascii_clock.py

--- Module 8/ascii_clock.py
+++ Module 8/ascii_clock.py
@@ -1,73 +1,42 @@
-colon = [[0],[1],[0],[1],[0]]
-a_list = [[0,1,0],[1,0,1],[1,1,1],[1,0,1],[1,0,1]]
-p_list = [[1,1,1],[1,0,1],[1,1,1],[1,0,0],[1,0,0]]
-m_list = [[1,0,0,0,1],[1,1,0,1,1],[1,0,1,0,1],[1,0,0,0,1],[1,0,0,0,1]]
-zero = [[1,1,1],[1,0,1],[1,0,1],[1,0,1],[1,1,1]]
-one = [[0,1,0],[1,1,0],[0,1,0],[0,1,0],[1,1,1]]
-two = [[1,1,1],[0,0,1],[1,1,1],[1,0,0],[1,1,1]]
-three = [[1,1,1],[0,0,1],[1,1,1],[0,0,1],[1,1,1]]
-four = [[1,0,1],[1,0,1],[1,1,1],[0,0,1],[0,0,1]]
-five = [[1,1,1],[1,0,0],[1,1,1],[0,0,1],[1,1,1]]
-six = [[1,1,1],[1,0,0],[1,1,1],[1,0,1],[1,1,1]]
-seven = [[1,1,1],[0,0,1],[0,0,1],[0,0,1],[0,0,1]]
-eight = [[1,1,1],[1,0,1],[1,1,1],[1,0,1],[1,1,1]]
-nine = [[1,1,1],[1,0,1],[1,1,1],[0,0,1],[1,1,1]]
-
 conversion = {
-  ":": colon,
-  "A": a_list,
-  "P": p_list,
-  "M": m_list,
-  "0": zero,
-  "1": one,
-  "2": two,
-  "3": three,
-  "4": four,
-  "5": five,
-  "6": six,
-  "7": seven,
-  "8": eight,
-  "9": nine
+  "0": ["000","0 0","0 0","0 0","000"],
+  "1": [" 1 ","11 "," 1 "," 1 ","111"],
+  "2": ["222","  2","222","2  ","222"],
+  "3": ["333","  3","333","  3","333"],
+  "4": ["4 4","4 4","444","  4","  4"],
+  "5": ["555","5  ","555","  5","555"],
+  "6": ["666","6  ","666","6 6","666"],
+  "7": ["777","  7","  7","  7","  7"],
+  "8": ["888","8 8","888","8 8","888"],
+  "9": ["999","9 9","999","  9","999"],
+  ":": [" ",":"," ",":"," "],
+  "A": [" A ","A A","AAA","A A","A A"],
+  "P": ["PPP","P P","PPP","P  ","P  "],
+  "M": ["M   M","MM MM","M M M","M   M","M   M"]
 }
 
-exceptions = [":", "A", "P", "M"]
-permitted_characters = "abcdeghkmnopqrsuvwxyz@$&*="
+time = input('Enter the time: ')
+clock_type = input('Choose the clock type (12 or 24): ')
+character = input('Enter your preferred character: ')
+while character not in "abcdeghkmnopqrsuvwxyz@$█&*= ":
+  character = input('Character not permitted! Try again: ')
 
-def military_to_twelve(military_time : str) -> str:
-  hours_minutes = military_time.split(":")
-
-  period = 'AM'
+if clock_type == "12":
+  hours_minutes = time.split(":")
   hour = int(hours_minutes[0])
 
+  period = 'AM' if hour < 12 else 'PM'
   if hour == 0:
     hour = 12
-  elif hour >= 13:
+  elif hour > 12:
     hour -= 12
 
-  if hour >= 12:
-    period = 'PM'
-    
-  hours_minutes[0] = str(hour)
-  return ':'.join(hours_minutes) + period
+  time = str(hour) + ":" + hours_minutes[1] + period
 
-def print_digits(digit_list : list):
-  for row in range(5):
-    for digit in digit_list:
-      if digit not in exceptions and user_character != "":
-        character = user_character
-      else:
-        character = digit
-      current_row = conversion[digit][row]
-      converted_row = [character if num == 1 else " " for num in current_row]
-      print(''.join(converted_row), end=" ")
-    print()
-
-time = input('Enter the time: ')
-if input('Choose the clock type (12 or 24): ') == "12":
-  time = military_to_twelve(time)
-
-user_character = input('Enter your preferred character: ')
-while user_character not in permitted_characters and user_character != "":
-  user_character = input('Character not permitted! Try again: ')
-digits = list(time)
-print_digits(digits)
+for row in range(5):
+  for digit in time:
+    digit_row = conversion[digit][row]
+    if digit != " " and digit not in ":APM":
+      digit_row = digit_row.replace(digit, character)
+    print(digit_row, end=" ")
+  print()
