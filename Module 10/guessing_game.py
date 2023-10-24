fc15e544bfa36566f53bc9f3291fe24c9d2bb5e7
Peter <49101240+peterphann@users.noreply.github.com>
2023-10-23 20:22:48 -0500

    Created guessing_game.py

---
+++ Module 10/guessing_game.py
@@ -0,0 +1,25 @@
+# By submitting this assignment, I agree to the following:
+#   "Aggies do not lie, cheat, or steal, or tolerate those who do."
+#   "I have not given or received any unauthorized aid on this assignment."
+#
+# Name:         Peter Phan
+# Section:      521
+# Assignment:   Lab 10.14 
+# Date:         23 October 2023
+
+def 
+
+def prompt_input():
+  try:
+    guess = int(input('What is your guess? '))
+  except:
+    print('Bad input! Tr')
+
+def main():
+  secret_number = 27
+  print('Guess the secret number! Hint: it\'s an integer between 1 and 100...')
+
+  print('Hi')
+
+if __name__ == "__main__":
+  main()
