3f6efd4ae3e63c9408f8cb6c18f9eaaf6e49ad51
Peter <49101240+peterphann@users.noreply.github.com>
2023-10-27 11:33:35 -0500

    Improved four squares algorithm

--- Module 10/sum_squares.py
+++ Module 10/sum_squares.py
@@ -8,28 +8,42 @@
 # Date:         23 October 2023
 
 from time import time
-from math import sqrt
+from math import sqrt, ceil
 
-# def list_nums(n):
-#     '''chatgpt solution'''
-#     for a in range(1, n):
-#         for b in range(a, n):
-#             for c in range(b, n):
-#                 for d in range(c, n):
-#                     if a**2 + b**2 + c**2 + d**2 == n:
-#                         return [a, b, c, d]
+def limit(n):
+  # Get limit of number
+  if n < 0:
+    return 0
+  else:
+    return ceil(sqrt(n)) + 1
 
 def list_nums(n):
-  nums = []
-  while n != 0:
-    nums += [int(sqrt(n))]
-    n -= int(sqrt(n)) ** 2
-  return nums
+  # Get maximum number that the term could potentially be
+  lim = limit(n)
+  # For each term, loop from 0 to the limit and see if it equals n
+  for a in range(0, lim):
+      for b in range(a, limit(n - a**2)):
+          for c in range(b, limit(n - a**2 - b**2)):
+              for d in range(c, limit(n - a**2 - b**2 - c**2)):
+                  if a**2 + b**2 + c**2 + d**2 == n:
+                      return [a, b, c, d]
+                     
+def count_sets(n):
+  # Same process as list_nums but keep track of valid numbers
+  count = 0
+  limit = ceil(sqrt(n)) + 1
+  for a in range(0, limit):
+      for b in range(a, limit):
+          for c in range(b, limit):
+              for d in range(c, limit):
+                  if a**2 + b**2 + c**2 + d**2 == n:
+                      count += 1
+  return count
 
-print(list_nums(15))
-
-# # how to measure how long your function takes to run:
-# t1 = time() # get start time
-# print(list_nums(15)) # run function
-# t2 = time() # get end time
-# print(f"This took {t2-t1} seconds") # print result
+# how to measure how long your function takes to run:
+t1 = time() # get start time
+print(list_nums(15)) # run function
+t2 = time() # get end time
+print(f"This took {t2-t1} seconds") # print result
+  
+print(count_sets(20000))
