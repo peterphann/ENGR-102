6c50c6afa9f6522e846e23984a9c9605877f894c
Peter <49101240+peterphann@users.noreply.github.com>
2023-11-01 18:26:37 -0500

    Created passport_checker.py

---
+++ Module 11/passport_checker.py
@@ -0,0 +1,47 @@
+# By submitting this assignment, I agree to the following:
+# "Aggies do not lie, cheat, or steal, or tolerate those who do."
+# "I have not given or received any unauthorized aid on this assignment."
+#
+# Names: Peter Phan
+# Olivia Goralski
+# Claudia Phan  
+# Benjamin Sandefur
+# Section: 521
+# Assignment: Lab 11.9
+# Date: 1 November 2023
+
+def is_valid(passport):
+  '''Returns a boolean value representing whether a given passport is valid'''
+  
+  checks = ['byr', 'cid', 'ecl', 'eyr', 'hcl', 'hgt', 'iyr', 'pid']
+  
+  # Clean passport string and isolate keys
+  passport = passport.replace('\n', " ")
+  pairs = passport.split(' ')
+  keys = [pair.split(':')[0] for pair in pairs]
+  
+  for key in keys:
+    if key in checks:
+      checks.remove(key)
+    else:
+      return False
+    
+  return (checks == [] or checks == ['ecl'])
+  
+# Open the file provided and get passports
+file_name = input('Enter the name of the file: ')
+file = open(file_name)
+passports = file.read().split('\n\n')
+file.close()
+
+# Loop through each passport and write to valid passports if valid
+output_file = open('valid_passports.txt', 'w')
+
+valid = 0
+for passport in passports:
+  if is_valid(passport):
+    valid += 1
+    output_file.write(passport + "\n\n")
+output_file.close()
+
+print(f'There are {valid} valid passports')
